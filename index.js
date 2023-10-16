8a50a46c4e2cc052fcb68858dd67e6d2b711af85
Faruk Çakal <fcakal07@gmail.com>
2023-10-16 13:41:43 +0300

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const fs = require("fs");
+
+puppeteer.use(StealthPlugin());
+
+var url = "https://www.mobilassistance.com/Paket-Satin-Al";
+
+  (async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        args: ['--start-maximized'] 
+      });
+    const page = await browser.newPage();
+  
+    await page.goto(url); // Sayfanın URL'sini buraya ekleyin
+  
+    const data = {}; // JSON verilerini depolamak için boş bir obje oluşturun
+  
+    // Select elementini bul
+    const selectElementContentPlaceHolder1 = await page.$('#ContentPlaceHolder1_TXT_AracModeli');
+  
+    // Select elementinin içindeki tüm seçenekleri al
+    const optionsContentPlaceHolder1 = await selectElementContentPlaceHolder1.$$eval('option', options => options.map(option => {
+      return {
+        value: option.value,
+        text: option.textContent.trim()
+      };
+    }));
+  
+    for (const option of optionsContentPlaceHolder1) {
+      // JSON objesine seçeneği ekleyin
+      data[option.text] = [];
+  
+      // Seçenekleri sırasıyla tıkla
+      await page.select('#ContentPlaceHolder1_TXT_AracModeli', option.value);
+  
+      // Sayfanın yeniden yüklenmesini bekle
+      await page.waitForNavigation();
+  
+      // Datalist elementinin değerlerini al
+      const datalistOptionsAracListe = await page.$$eval('#AracListe option', options => options.map(option => option.value));
+  
+      // JSON objesine datalist değerlerini ekleyin
+      data[option.text] = datalistOptionsAracListe;
+    }
+  
+    // JSON verilerini bir dosyaya yazın
+    fs.writeFileSync('veriler.json', JSON.stringify(data, null, 2));
+  
+    console.log('Veriler başarıyla JSON dosyasına yazıldı: veriler.json');
+  
+    await browser.close();
+  })();
